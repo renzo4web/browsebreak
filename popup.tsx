94acabaf2c439013ba7c14eb084f85201e7f2626
Renzo Barrios <renzo@bloqhab.xyz>
2024-02-03 15:05:45 -0300

    feat: themes, popup styled v2, clean urls,active, delete url

--- popup.tsx
+++ popup.tsx
@@ -4,12 +4,18 @@ import "./style.css"
 
 const query = { active: true, currentWindow: true }
 
+function cleanUrl(url: string) {
+  const urlObj = new URL(url)
+  return urlObj.origin + urlObj.pathname
+}
+
 function IndexPopup() {
   const [sites, setSites] = useStorage("sites")
 
-  const saveSite = async (url: string) => {
+  const saveSite = async (urlIn: string) => {
+    const url = cleanUrl(urlIn)
     if (url && !sites?.find((site) => site.url === url)) {
-      await setSites([...(sites ?? []), { url }])
+      await setSites([...(sites ?? []), { url, active: true }])
       alert("Saved, refresh the page to see the changes")
     } else {
       alert("Site already saved")
@@ -24,13 +30,25 @@ function IndexPopup() {
 
   return (
     <div
-      style={{ width: "100%" }}
-      className="w-11/12 h-full bg-gray-2  min-w-96 px-5 py-5 flex flex-col items-center">
-      <div className="w-full">
+      style={{
+        width: "100%"
+      }}
+      className="bg-gray-2 ring-2  min-w-96 px-3 py-5 flex items-center flex-col">
+      <div className="mx-auto  w-full">
         <form onSubmit={handleSubmit}>
-          <input className="input-block input" placeholder="site url" />
+          <input
+            style={{
+              width: "100%",
+              minWidth: "100%"
+            }}
+            className="input-solid input input-lg"
+            type={"url"}
+            placeholder="Site URL"
+          />
           <div className={"space-x-4 py-4"}>
-            <button className={"btn btn-primary btn-xs"} type="submit">
+            <button
+              className={"btn btn-rounded btn-primary btn-md"}
+              type="submit">
               Save
             </button>
 
@@ -41,7 +59,7 @@ function IndexPopup() {
                   await saveSite(currentTab?.url)
                 })
               }}
-              className={"btn btn-outline-secondary btn-xs"}>
+              className={"btn btn-rounded btn-outline-secondary btn-md"}>
               Add current site
             </button>
           </div>
@@ -53,14 +71,10 @@ function IndexPopup() {
         <label
           htmlFor="toggle-15"
           className="accordion-title bg-gray-2 text-md text-gray-11">
-          Current sites
+          Saved sites
         </label>
         <span className="accordion-icon">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24">
+          <svg width="24" height="24" viewBox="0 0 24 24">
             <path d="M13.293 6.293 7.586 12l5.707 5.707 1.414-1.414L10.414 12l4.293-4.293z"></path>
           </svg>
         </span>
@@ -74,20 +88,45 @@ function IndexPopup() {
                   <h4 className="text-md truncate max-w-64 text-gray-11">
                     {site?.url}
                   </h4>
-                  <input
-                    type="checkbox"
-                    className="switch switch-success "
-                    checked={!!site?.active}
-                    onChange={async (e) => {
-                      const newSites = sites?.map((s: any) => {
-                        if (s.url === site.url) {
-                          s.active = e.target.checked
-                        }
-                        return s
-                      })
-                      await setSites(newSites)
-                    }}
-                  />
+                  <div className="inline-flex items-center space-x-1">
+                    <input
+                      type="checkbox"
+                      className="switch switch-success "
+                      checked={!!site?.active}
+                      onChange={async (e) => {
+                        const newSites = sites?.map((s: any) => {
+                          if (s.url === site.url) {
+                            s.active = e.target.checked
+                          }
+                          return s
+                        })
+                        await setSites(newSites)
+                      }}
+                    />
+
+                    <button
+                      onClick={async () => {
+                        const newSites = sites?.filter(
+                          (s: any) => s.url !== site.url
+                        )
+                        await setSites(newSites)
+                      }}
+                      className="btn  btn-circle">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="h-4 w-4"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor">
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth="2"
+                          d="M6 18L18 6M6 6l12 12"
+                        />
+                      </svg>
+                    </button>
+                  </div>
                 </div>
               )
             })}
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,5 +3,25 @@ module.exports = {
   mode: "jit",
   darkMode: "class",
   content: ["./**/*.tsx"],
-  plugins: [require("@tailwindcss/forms"), require("rippleui")]
+  plugins: [require("@tailwindcss/forms"), require("rippleui")],
+  rippleui: {
+    themes: [
+      {
+        themeName: "light",
+        colorScheme: "light",
+        colors: {
+          primary: "#0069ed",
+          secondary: "#042A2B"
+        }
+      },
+      {
+        themeName: "dark",
+        colorScheme: "dark",
+        colors: {
+          primary: "#0069ed",
+          secondary: "#F4E04D"
+        }
+      }
+    ]
+  }
 }
